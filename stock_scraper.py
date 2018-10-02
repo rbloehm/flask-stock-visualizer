2b93f02fbdbc2b5626227377ef1c12ca0ddd232d
Robin <robin.bloehm@gmail.com>
2018-10-02 19:49:29 +0200

    use my solution

--- stock_scraper.py
+++ stock_scraper.py
@@ -1,24 +1,11 @@
-import csv
-import requests
-
-
-URL = "http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download"
+import pandas as pd
 
 
 def get_data():
-    r = requests.get(URL)
-    data = r.text
-    RESULTS = {'children': []}
-    for line in csv.DictReader(data.splitlines(), skipinitialspace=True):
-        if float(line['Nasdaq100_points']) > .01:
-            RESULTS['children'].append({
-                'name': line['Name'],
-                'symbol': line['Symbol'],
-                'symbol': line['Symbol'],
-                'price': line['lastsale'],
-                'net_change': line['netchange'],
-                'percent_change': line['pctchange'],
-                'volume': line['share_volume'],
-                'value': line['Nasdaq100_points']
-            })
-    return RESULTS
+    url = "http://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download"
+    df = pd.read_csv(url, skipinitialspace=True, header=0,
+                     names=["symbol", "name", "current_price", "net_change",
+                            "percent_change", "volume", "value", "skip"])
+    df.drop(columns="skip", inplace=True)
+    df = df[df.value > 0.01]
+    return {"children": df.to_dict("records")}
